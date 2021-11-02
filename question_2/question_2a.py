c0b1ee851eef49af072d3347b7e36628945a6b6e
Wan Ab Muadz Adzim <muadz.nagasolo@gmail.com>
2021-11-02 16:01:11 +0000

    modified; solution to question 2a

--- question_2/question_2a.py
+++ question_2/question_2a.py
@@ -5,6 +5,7 @@
     - Added logic to method deposit() and withdraw()
     - Instantiate 3 instances
     - Store instances with namedtuple
+    - Store instances with hash maps
 
 """
 
@@ -34,11 +35,16 @@ if __name__ == '__main__':
     account1 = Account('Boris', 20)
     account2 = Account('Borhan', 30)
     account3 = Account('Borat', 20)
-    
-    # store instance using data structure namedtuple
-    from collections import namedtuple
-    account = namedtuple('Account', ('ID Name Balance'))
 
-    boris_account = account(str(account1.id), account1.name, account1.balance)
-    borhan_account = account(str(account2.id), account2.name, account2.balance)
-    borat_account = account(str(account3.id), account3.name, account3.balance)
+    # store instance as hashmap
+    accounts = {acc.id:acc for acc in [account1, account2, account3]}
+    print(accounts.keys())
+    print(accounts.values())
+
+    # store instance using data structure namedtuple
+    # from collections import namedtuple
+    # account = namedtuple('Account', ('ID Name Balance'))
+
+    # boris_account = account(str(account1.id), account1.name, account1.balance)
+    # borhan_account = account(str(account2.id), account2.name, account2.balance)
+    # borat_account = account(str(account3.id), account3.name, account3.balance)
